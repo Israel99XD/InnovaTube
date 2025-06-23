e2e7b65dcafdf035c94b0c79aeaa374b048f7dcd
Israel Saldaña <israelsldn@gmail.com>
2025-06-22 19:00:26 -0600

    Corregi errores al fusionar ramas

--- InnovaT/src/app/components/home/home.component.ts
+++ InnovaT/src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [InputIcon, IconField, InputTextModule, FormsModule],
+  imports: [InputTextModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
